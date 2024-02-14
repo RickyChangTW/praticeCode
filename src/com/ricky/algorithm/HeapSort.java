d2841cf424e0d68ed2a5c4452f2343d5bb07c1f3
Ricky <no791106@gmail.com>
2024-02-14 16:12:41 +0800

    firstCommit:
    revise:
    初次建立

---
+++ src/com/ricky/algorithm/HeapSort.java
@@ -0,0 +1,17 @@
+package com.ricky.algorithm;
+
+public class HeapSort {
+    static int[] target = new int[]{8, 3, 13, 12, 7, 6, 21, 18, 55, 85, 24, 89, 14};
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void buildMaxHeap(){
+
+    }
+
+    public static void maxHeapify(int index){
+
+    }
+}

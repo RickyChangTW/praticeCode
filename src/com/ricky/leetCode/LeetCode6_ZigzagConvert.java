aec2df85c09408e468d796113a492ebacfb8df81
Ricky <no791106@gmail.com>
2024-02-15 21:37:13 +0800

    20240215-1
    revise:
    complete leetcode6

--- src/com/ricky/leetCode/LeetCode6_ZigzagConvert.java
+++ src/com/ricky/leetCode/LeetCode6_ZigzagConvert.java
@@ -4,30 +4,30 @@ import java.util.ArrayList;
 
 public class LeetCode6_ZigzagConvert {
     public static void main(String[] args) {
-        System.out.printf(convert("ABCDEFGHIJ",4));
-
+        System.out.printf(convert("PAYPALISHIRING", 4));
     }
 
     public static String convert(String s, int numRows) {
+        if(numRows == 1 )return s;
         int cycleLength = numRows * 2 - 2;
         String result = "";
         ArrayList<String> cycleStringList = new ArrayList<>();
-        for(int i = 0;i<numRows ;i++){
+        for (int i = 0; i < numRows; i++) {
             cycleStringList.add("");
         }
         for (int i = 0; i < s.length(); i++) {
             int cycleindex = i % cycleLength;
             int index = 0;
-            if(cycleindex <= numRows){
-                index = i;
-            }else{
-                index = 2 * numRows - cycleindex - 1;
+            if (cycleindex < numRows) {
+                index = cycleindex;
+            } else {
+                index = 2 * numRows - cycleindex - 2;
             }
             String s1 = cycleStringList.get(index);
             s1 += s.charAt(i);
-            cycleStringList.add(index,s1 );
+            cycleStringList.set(index, s1);
         }
-        for(int i = 0;i<numRows ;i++){
+        for (int i = 0; i < numRows; i++) {
             result += cycleStringList.get(i);
         }
 
